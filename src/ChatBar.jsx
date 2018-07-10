0cbbc60e83d14f3bbd4d16b6654140cca6f36686
Eliza Moore <elzmoore13@gmail.com>
2018-07-10 21:04:59 +0000

    display messages and add new message to display

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,11 +1,30 @@
 import React, {Component} from 'react';
 
 class ChatBar extends Component{
+  // to generate unique ids
+  generateUniqueId(){
+   const uniqueKey = (Math.random()*1000000000).toFixed(0);
+   return uniqueKey;
+  }
+
+  sendMessageUp(event){
+    if (event.keyCode == 13) {
+      const newMessageObject = {
+        id: this.generateUniqueId(),
+        username: document.getElementById('user').value,
+        content: event.target.value,
+      }
+      event.target.value = '';
+      this.props.sendMessage(newMessageObject);
+    } else {
+      return false;
+    }
+  }
   render(){
     return(
       <footer className="chatbar">
-        <input className="chatbar-username" placeholder="Your Name (Optional)" />
-        <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
+        <input id='user' className="chatbarUsername" type='text' defaultValue={this.props.currentUser.name}/>
+        <input onKeyDown={this.sendMessageUp.bind(this)} className="chatbarMessage" placeholder="Type a message and hit ENTER" />
       </footer>
     );
   }
--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -1,15 +1,17 @@
 import React, {Component} from 'react';
 import Message from './Message.jsx';
 
+function makeMessage(messages) {
+  const multiMessages = messages.map((message) =>{
+    return(<Message key={message.id} message={message} />)
+  })
+  return multiMessages;
+}
+
 class MessageList extends Component{
   render() {
-    return(<main className="messages">
-              <Message />
-              <div className="message system">
-                Anonymous1 changed their name to nomnom.
-              </div>
-            </main>);
-         }
+    return(<main className='messages'>{makeMessage(this.props.messages)} <div className='message system'> </div> </main>);
+  }
 
 }
 
